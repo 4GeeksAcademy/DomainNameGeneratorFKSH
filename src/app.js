63ccedccdb56a341bfd45469a4c8cfc61500fce3
Frank Kenneth Spacey Helder <franksh14@gmail.com>
2024-05-20 09:32:09 +0000

    DomainNameGeneratorfksh

--- src/app.js
+++ src/app.js
@@ -1,11 +1,29 @@
-/* eslint-disable */
-import "bootstrap";
-import "./style.css";
+let pronoun = ["the", "our", "your", "their", "his", "her", "those", "my"];
+let adj = [
+  "huge",
+  "big",
+  "massive",
+  "great",
+  "mackoff",
+  "grand",
+  "small",
+  "ridiculous"
+];
+let noun = [
+  "jogger",
+  "racoon",
+  "coder",
+  "geek",
+  "programmer",
+  "boffin",
+  "nerd",
+  "cat"
+];
 
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
-
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
-};
+for (let i = 0; i < pronoun.length; i++) {
+  for (let j = 0; j < adj.length; j++) {
+    for (let f = 0; f < noun.length; f++) {
+      console.log(`${pronoun[i]}${adj[j]}${noun[f]}.com`);
+    }
+  }
+}
